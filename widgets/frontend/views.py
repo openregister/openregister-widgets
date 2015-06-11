948c5ef2e3d5e4854a8aa5966876d077449c5fba
Adam Shimali <adam.shimali@gmail.com>
2015-06-11 17:00:57 +0100

    just send entry to page

--- widgets/frontend/views.py
+++ widgets/frontend/views.py
@@ -19,7 +19,8 @@ def country_picker():
     country_register = current_app.config['COUNTRY_REGISTER']
     country_register_url = "%s/all.json" % country_register
     res = requests.get(country_register_url, headers={"Content-type": "application/json"})
-    countries = res.json()
+    countries_json = res.json()
+    countries = [country['entry'] for country in countries_json]
     current_app.logger.info(countries)
     return render_template('country_picker.html', countries=countries, country_register=country_register)
 
