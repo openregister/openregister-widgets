b0524fe0c4bc60689543ec8a6d33d9b26921704c
Adam Shimali <adam.shimali@gmail.com>
2015-06-16 15:15:18 +0100

    A non working sort of addresses

--- widgets/static/javascripts/address-lookup.js
+++ widgets/static/javascripts/address-lookup.js
@@ -2,39 +2,45 @@ var addressLookup = function(event) {
     var input = $("#address-lookup :input[name='search']"),
         searchValue = input.val().trim();
     event.preventDefault();
-    $('.results').empty();
+    $('#address-results').hide();
+    $('#results').empty();
     $('.message').empty();
-
 
     $.ajax({
           type: 'GET',
           url: 'http://address.openregister.org/search?_query='+searchValue+'&_representation=json',
           contentType: 'application/json',
           success: function(data) {
-            renderAddresses(data);
-            $('#address-results').show();
+            if(data.length > 1000){
+                $('.message').text('Too many results. Search for for something more specific.');
+            } else {
+                renderAddresses(data);
+                $('#address-results').show();
+            }
           },
           error: function(xhr, options, error) {
             console.log(error);
-            $('.results').empty();
+            $('#results').empty();
             $('.message').text('No results');
         }
     });
-
 };
 
 var renderAddresses = function(addresses) {
     //TODO get server to sort results
+    addresses.sort(function(a,b) {
+        return a.entry.street.localeCompare(b.entry.street);
+    });
     $('#results').append("<option value=\"\">" + addresses.length + " addresses found</option>");
     $.each(addresses, function(index, address) {
         var template = $.templates("#address-template"),
             html = template.render({
-                    'address': address.entry.address,
-                    'property': address.entry.property,
-                    'street': address.entry.street,
-                    'town': address.entry.town,
-                    'postcode': address.entry.postcode
-                });
+                'address': address.entry.address,
+                'property': address.entry.property,
+                'street': address.entry.street,
+                'town': address.entry.town,
+                'postcode': address.entry.postcode
+            });
         $('#results').append(html);
     });
 };
