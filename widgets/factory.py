372398a4955934fa10bf6619a584afacde24d07b
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-10-15 15:06:21 +0100

    another typo

--- widgets/factory.py
+++ widgets/factory.py
@@ -18,10 +18,10 @@ def create_app(config_filename):
 
 def register_errorhandlers(app):
     def render_error(error):
+        app.logger.error(error)
         # If a HTTPException, pull the `code` attribute; default to 500
         error_code = getattr(error, 'code', 500)
         return render_template("{0}.html".format(error_code)), error_code
-    app.logger.error(error)
     for errcode in [401, 404, 500]:
         app.errorhandler(errcode)(render_error)
     return None
